fa981b1da7796faefb7888e965ae976052514c7f
madhih <madhih@proton.me>
2025-12-20 00:26:29 +0530

    made some major changes

--- script.js
+++ script.js
@@ -63,14 +63,14 @@ const cells = document.querySelectorAll(".cell");
 const statusDisplay = document.querySelector(".announcement");
 const game = new GameBoard();
 
-const cellReset = async () => {
-    await sleep(2000);
+const cellReset = async (waitTime) => {
+    await sleep(waitTime);
     game.boardReset();
     // cells.forEach((cell) => (cell.innerText = ""));
     cells.forEach((cell) => {
         cell.classList.remove("x-marker", "o-marker");
     });
-    statusDisplay.innerText = "New Game! Player X starts.";
+    statusDisplay.innerText = "Player X starts.";
     game.switchStatus();
 };
 
@@ -93,7 +93,9 @@ async function handleCellClick(clickedCellEvent) {
                 draw: "Game ended in a draw!",
             };
             statusDisplay.innerText = messages[result];
-            await cellReset();
+            statusDisplay.classList.add(`${result}-win`);
+            await cellReset(2000);
+            statusDisplay.classList.remove(`${result}-win`);
         } else {
             game.switchplayer();
             statusDisplay.innerText = `Player ${
